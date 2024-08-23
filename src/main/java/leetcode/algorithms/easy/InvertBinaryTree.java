591af2f6e1b6ca50bd8fde71aa5a69f2a630702d
Chloe <yunyunya@gmail.com>
2024-08-24 00:50:19 +0200

    226. Invert Binary Tree

---
+++ src/main/java/leetcode/algorithms/easy/InvertBinaryTree.java
@@ -0,0 +1,32 @@
+package leetcode.algorithms.easy;
+
+public class InvertBinaryTree {
+
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
