ff4a494ef64526eb07885da1a2eaa94b16d898d0
Chloe <yunyunya@gmail.com>
2024-08-29 12:47:49 +0200

    938. Range Sum of BST

---
+++ src/main/java/leetcode/algorithms/easy/RangeSumOfBST.java
@@ -0,0 +1,20 @@
+package leetcode.algorithms.easy;
+
+import leetcode.algorithms.TreeNode;
+
+// 938. Range Sum of BST
+public class RangeSumOfBST {
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+}
