04c0f7304e61f13a8c593d5176962bd66cf1cef6
Chloe <yunyunya@gmail.com>
2024-09-13 01:16:41 +0200

    141. Linked List Cycle

---
+++ src/main/java/leetcode/algorithms/easy/LinkedListCycle.java
@@ -0,0 +1,20 @@
+package leetcode.algorithms.easy;
+
+import leetcode.algorithms.ListNode;
+
+// 141. Linked List Cycle
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
