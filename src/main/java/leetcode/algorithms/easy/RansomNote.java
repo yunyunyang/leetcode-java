a7d5b7574c6d1d67827659af657e1487504afff4
Chloe <yunyunya@gmail.com>
2024-09-26 23:44:16 +0200

    383. Ransom Note

---
+++ src/main/java/leetcode/algorithms/easy/RansomNote.java
@@ -0,0 +1,25 @@
+package leetcode.algorithms.easy;
+
+import java.util.HashMap;
+
+// 383. Ransom Note
+public class RansomNote {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < magazine.length(); i++) {
+            char c = magazine.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char c = ransomNote.charAt(i);
+            if (map.containsKey(c) && map.get(c) > 0) {
+                map.put(c, map.get(c) - 1);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
