ade1779c6d63cce0ae9d0e45ce0f78e6c9dd7817
Chloe <yunyunya@gmail.com>
2024-12-09 12:42:14 +0100

    3151. Special Array I

---
+++ src/main/java/leetcode/algorithms/easy/SpecialArrayI.java
@@ -0,0 +1,14 @@
+package leetcode.algorithms.easy;
+
+// 3151. Special Array I
+public class SpecialArrayI {
+
+    public boolean isArraySpecial(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (((nums[i - 1] & 1) ^ (nums[i] & 1)) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
