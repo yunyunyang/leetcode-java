855d56ff632d8838815c3009c917e4dbd7df0e4d
Chloe <yunyunya@gmail.com>
2025-11-21 22:45:09 +0100

    567. Permutation in String

---
+++ src/main/java/leetcode/algorithms/medium/PermutationInString.java
@@ -0,0 +1,47 @@
+package leetcode.algorithms.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 567. Permutation in String
+public class PermutationInString {
+
+    public boolean checkInclusion(String s1, String s2) {
+        Map<Character, Integer> need = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        Map<Character, Integer> window = new HashMap<>();
+        int left = 0, right = 0, valid = 0;
+        while (right < s2.length()) {
+            char c = s2.charAt(right);
+            right++;
+
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+
+            while (right - left >= s1.length()) {
+                if (valid == need.size()) {
+                    return true;
+                }
+
+                char d = s2.charAt(left);
+                left++;
+
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+            }
+        }
+        return false;
+    }
+}
