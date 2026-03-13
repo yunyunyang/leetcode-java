2d3329816f1659773ed26d063d1e57061ea5cbd2
Chloe <yunyunya@gmail.com>
2026-03-13 14:14:49 +0100

    77. Combinations

---
+++ src/main/java/leetcode/algorithms/medium/Combinations.java
@@ -0,0 +1,29 @@
+package leetcode.algorithms.medium;
+
+import java.util.LinkedList;
+import java.util.List;
+
+// 77. Combinations
+public class Combinations {
+
+    List<List<Integer>> res = new LinkedList<>();
+    LinkedList<Integer> track = new LinkedList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        backtrack(1, n, k);
+        return res;
+    }
+
+    void backtrack(int start, int n, int k) {
+        if (track.size() == k) {
+            res.add(new LinkedList<>(track));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            track.addLast(i);
+            backtrack(i + 1, n, k);
+            track.removeLast();
+        }
+    }
+}
