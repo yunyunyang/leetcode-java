4c16e14c59fddec6d7e444472d03c445f5447faa
Chloe <yunyunya@gmail.com>
2026-05-05 22:48:53 +0200

    867. Transpose Matrix

---
+++ src/main/java/leetcode/algorithms/easy/TransposeMatrix.java
@@ -0,0 +1,16 @@
+package leetcode.algorithms.easy;
+
+// 867. Transpose Matrix
+public class TransposeMatrix {
+
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] res = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+}
