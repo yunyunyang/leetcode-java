7c985d2be551934fc898919da2753dce3659b119
Chloe <yunyunya@gmail.com>
2026-05-16 19:49:20 +0200

    387. First Unique Character in a String

---
+++ src/main/java/leetcode/algorithms/easy/FirstUniqueCharacterInAString.java
@@ -0,0 +1,20 @@
+package leetcode.algorithms.easy;
+
+// 387. First Unique Character in a String
+public class FirstUniqueCharacterInAString {
+
+    public int firstUniqChar(String s) {
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            count[c - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (count[c - 'a'] == 1)
+                return i;
+        }
+        return -1;
+    }
+}
