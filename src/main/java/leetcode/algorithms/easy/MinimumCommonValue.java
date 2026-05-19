473fdd23f6525789faec383a9efcf5bbeeaf3f06
Chloe <yunyunya@gmail.com>
2026-05-19 19:36:57 +0200

    2540. Minimum Common Value

---
+++ src/main/java/leetcode/algorithms/easy/MinimumCommonValue.java
@@ -0,0 +1,19 @@
+package leetcode.algorithms.easy;
+
+// 2540. Minimum Common Value
+public class MinimumCommonValue {
+
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums2[j] < nums1[i]) {
+                j++;
+            }
+        }
+        return -1;
+    }
+}
