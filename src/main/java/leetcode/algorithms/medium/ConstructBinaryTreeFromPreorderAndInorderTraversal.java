02956d9b36e2eab6b446c3a9c3bba27990ff03da
Chloe <yunyunya@gmail.com>
2026-05-30 10:39:13 +0200

    105. Construct Binary Tree from Preorder and Inorder Traversal

--- src/main/java/leetcode/algorithms/medium/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/main/java/leetcode/algorithms/medium/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -3,28 +3,43 @@ package leetcode.algorithms.medium;
 import leetcode.algorithms.TreeNode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 // 105. Construct Binary Tree from Preorder and Inorder Traversal
 public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
 
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder.length == 0 || inorder.length == 0) return null;
+    Map<Integer, Integer> map = new HashMap<>();
 
-        TreeNode root = new TreeNode(preorder[0]);
-        int mid = 0;
-        for (int i = 1; i < inorder.length; i++) {
-            if (inorder[i] == preorder[0]) {
-                mid = i;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return build(preorder, 0, preorder.length - 1,
+                inorder, 0, inorder.length - 1);
+    }
+
+    TreeNode build(int[] preorder, int preStart, int preEnd,
+                   int[] inorder, int inStart, int inEnd) {
+
+        if (preStart > preEnd)
+            return null;
+
+        int rootVal = preorder[preStart];
+        int index = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == rootVal) {
+                index = i;
             }
         }
-        root.left = buildTree(
-                Arrays.copyOfRange(preorder, 1, mid + 1),
-                Arrays.copyOfRange(inorder, 0, mid)
-        );
-        root.right = buildTree(
-                Arrays.copyOfRange(preorder, mid + 1, preorder.length),
-                Arrays.copyOfRange(inorder, mid + 1, inorder.length)
-        );
+        int leftSize = index - inStart;
+
+        TreeNode root = new TreeNode(rootVal);
+        root.left = build(preorder, preStart + 1, preStart + leftSize,
+                inorder, inStart, inStart + leftSize);
+        root.right = build(preorder, preStart + leftSize + 1, preEnd,
+                inorder, index + 1, inEnd);
+
         return root;
     }
 }
